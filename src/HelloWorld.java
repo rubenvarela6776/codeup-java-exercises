120575541b3d30c1893551ba25f832ebbdd3c9df
rubenvarela <rubenv6776@gmail.com>
2017-01-03 12:12:26 -0600

    Created this new Java exercises repository and created new files such as my .gitignore file.

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by rubenvarela on 1/3/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("All java code lives indside of classes");
+        System.out.println("Our program runs the 'main' method when executed");
+        System.out.println("Goodbye World!");
+    }
+}
